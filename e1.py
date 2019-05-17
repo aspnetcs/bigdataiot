f36f491d4cb6867f0d409fc066da08bd4427c1c5
aspnetcs <aspnetcs@163.com>
2019-05-17 19:06:25 +0800

    Add files via upload

---
+++ e1.py
@@ -0,0 +1,15 @@
+﻿#爬取豆瓣书籍
+\# -*- coding: utf-8 -*-
+import requests
+from bs4 import BeautifulSoup
+from urllib import request
+url = "http://www.douban.com/tag/%E5%B0%8F%E8%AF%B4/?focus=book" #网页地址
+res = request.urlopen(url) #打开连接
+response = requests.get(url)#返回http状态响应码
+http_status_code = response.status_code
+print(http_status_code)
+soup = BeautifulSoup(res,"html.parser")
+book_div = soup.find(attrs={"id":"book"})
+book_a = book_div.findAll(attrs={"class":"title"})
+for book in book_a:
+    print (book.string)
