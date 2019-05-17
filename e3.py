2585676fbb61318118e38321a41a8aa9e165dfb4
aspnetcs <aspnetcs@163.com>
2019-05-17 19:13:01 +0800

    Add files via upload

---
+++ e3.py
@@ -0,0 +1,10 @@
+﻿#爬取Discuz帖子内容及连接
+from bs4 import BeautifulSoup
+import requests
+for i in range(200):
+    html = requests.get('http://114.112.74.138/forum.php?mod=viewthread&tid='+str(i))
+    soup = BeautifulSoup(html.text,"html.parser")
+    url = soup.link['href']
+    content = soup.find(attrs={"class":"t_fsz"})
+    print(url)
+    print(content)
